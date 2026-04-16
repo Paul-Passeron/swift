4a525e508e116529e3c797abd72d8dd16738a971
Paul Passeron <paul_passeron@apple.com>
2026-04-16 17:17:30 +0100

    Started working on turning Builtin Equatable* derivation macros into pure Swift macros

--- stdlib/public/core/DerivedConformance.swift
+++ stdlib/public/core/DerivedConformance.swift
@@ -1,14 +1,35 @@
-#if $Macros && hasAttribute(attached)
+@attached(body)
+public macro EquatableStructMacro() = Builtin.DerivedConformanceMacro
 
-  @attached(body)
-  public macro EquatableStructMacro() = Builtin.DerivedConformanceMacro
+@attached(body)
+public macro EquatableEnumMacro() = Builtin.DerivedConformanceMacro
 
-  @attached(body)
-  public macro EquatableEnumMacro() = Builtin.DerivedConformanceMacro
+@attached(member, names: arbitrary)
+public macro EquatableDeclMacroOld() = Builtin.DerivedConformanceMacro
 
-  @attached(member, names: arbitrary)
-  public macro EquatableDeclMacroOld() = Builtin.DerivedConformanceMacro
+@freestanding(declaration, names: named(__derived_equals))
+public macro EquatableDeclMacro() = Builtin.DerivedConformanceMacro
 
-  @freestanding(declaration, names: named(__derived_equals))
-  public macro EquatableDeclMacro() = Builtin.DerivedConformanceMacro
-#endif  // $Macros && hasAttribute(attached)
+@frozen public struct EnumCaseInfo {
+  let caseName: String
+  let argLabels: [String?]
+  let isUnavailable: Bool
+}
+
+extension EnumCaseInfo {
+  public static func new(caseName: String, argLabels: [String?] = [], isUnavailable: Bool = false)
+    -> Self
+  {
+    Self(caseName: caseName, argLabels: argLabels, isUnavailable: isUnavailable)
+  }
+
+}
+
+@frozen public enum DerivedNominalKind {
+  case aStruct(members: [String])
+  case anEnum(cases: [EnumCaseInfo])
+}
+
+@freestanding(declaration, names: named(__derived_equals))
+public macro deriveEquatable(_ kind: DerivedNominalKind) =
+  #externalMacro(module: "SwiftMacros", type: "DeriveEquatableMacro")
