52ac4131af0ec242d4bd7170b575a095492a8ad7
Paul Passeron <paul_passeron@apple.com>
2026-05-20 11:41:27 +0100

    [stdlib] Add _deriveEquatable freestanding macro

---
+++ stdlib/public/core/DerivedConformances.swift
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+/// This macro is used by the compiler to synthesize a conformance to the
+/// `Equatable` protocol from type information passed as \p info.
+@freestanding(declaration, names: named(__derived_equals))
+public macro _deriveEquatable(_ info: String) =
+  #externalMacro(module: "SwiftMacros", type: "DeriveEquatableMacro")
