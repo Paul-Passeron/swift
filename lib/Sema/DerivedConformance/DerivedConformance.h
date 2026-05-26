fe5b7fc53dcdcdb7aaf2361c1ddf6f5bbec7891d
Paul Passeron <paul_passeron@apple.com>
2026-05-26 13:21:56 +0100

    [Sema] Add API for deriving conformances via macros.
    This commit introduces the `deriveRequirementViaMacro` function taking a
    derived conformance, a requirement and a string representation of the macro
    declaration producing the witness and returns the witness.

--- lib/Sema/DerivedConformance/DerivedConformance.h
+++ lib/Sema/DerivedConformance/DerivedConformance.h
@@ -322,7 +322,7 @@ public:
   /// Derive a Decodable requirement for a struct type.
   ///
   /// \returns the derived member, which will also be added to the type.
-  ValueDecl *deriveDecodable(ValueDecl *requirement);  
+  ValueDecl *deriveDecodable(ValueDecl *requirement);
 
   /// Whether we can derive the given DistributedActor requirement in the given context.
   static bool canDeriveDistributedActor(NominalTypeDecl *nominal,
@@ -464,6 +464,11 @@ public:
 /// stored properties of the given nominal type, require actor isolation
 /// because they involve mutable state.
 bool memberwiseAccessorsRequireActorIsolation(NominalTypeDecl *nominal);
+
+/// Returns the value decl expanded from the macro in `code` in the context of
+/// the \p derived derived conformance for the \p requirement requirement.
+ValueDecl *deriveRequirementViaMacro(DerivedConformance &derived,
+                                     ValueDecl *requirement, StringRef code);
 
 } // namespace swift
 
